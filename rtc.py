7cba600dcfa9a46c3d6fe96b8e8efce365b6840f
Gabe Stein <gabe@ghscommunications.com>
2015-05-16 13:00:25 -0400

    naive color generation based on pos tagger

--- rtc.py
+++ rtc.py
@@ -1,28 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# import matplotlib.pyplot as p
 import nltk
-import rtmidi
-import time
-
-def play(out, note, ch, vel, sleep):
-
-    # 143 and 127 are ((MIDI channel 1 hex value) - 1)
-    # for turning a note on, and off respectively
-    # Thus, 143 + channel N = MIDI channel N
-
-    channel_on = 143 + ch
-    channel_off = 127 + ch
-    note_on = [channel_on, note, vel]
-    note_off = [channel_off, note, vel]
-    out.send_message(note_on)
-    time.sleep(sleep)
-    out.send_message(note_off)
+from wavelen2rgb import wavelen2rgb
 
 def score(text):
     notes = []
     sents = nltk.corpus.gutenberg.sents(text)
-    for sent in sents[100:200]:
+    for sent in sents[100:118]:
         # Note = [sentence length, noun count, verb count, adjective count]
         note = [len(sent), 0, 0, 0]
         notes.append(note)
@@ -44,20 +30,18 @@ def score(text):
     return notes
 
 def main():
-    in_ = 'melville-moby_dick.txt'
-    out = rtmidi.MidiOut()
+    in_ = 'carroll-alice.txt'
+    html_out = 'background: linear-gradient(180deg, '
     sheet = score(in_)
-    available_ports = out.get_ports()
-    if available_ports:
-        out.open_port(0)
-    else:
-        out.open_virtual_port("My virtual output")
+    wave_range = range(378,781)
     for note in sheet:
-        midi_note = (5 * note[1]) + (2 * note[2]) + (note[3]) + 21
-        velocity = note[0] + 100
-        sleep = note[0] * .02
-        play(out, midi_note, 2, velocity, sleep)
-    del out
-
+        # very naively transposes the note to an index between 1 and 400,
+        # the number of rbg wavelengths in the wave2rbg function.
+        wave_index = int(round(400 * ((note[1] + note[2] + note[3]) / 100.0)))
+        wavelen = wave_range[wave_index]
+        rgb = wavelen2rgb(wavelen)
+        html_out += "rgba(" + str(rgb[0]) + ", " + str(rgb[1]) + ", " + str(rgb[2]) + ", 1), "
+    html_out += ');'
+    print html_out
 if __name__ == "__main__":
     main()
