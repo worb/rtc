3ed4e85346d0c679f265ff843ce503c4f3c99e6b
Gabe Stein <gabe@ghscommunications.com>
2015-05-23 16:21:52 -0400

    use resizd book covers

--- www/src/js/Coverflow.js
+++ www/src/js/Coverflow.js
@@ -4,8 +4,13 @@ var Internal = require('./Internal');
 var Coverflow = React.createClass({
   getInitialState: function() {
     return {
-      selected: null
+      selected: null,
+      index: 0
     }
+  },
+  componentWillMount: function() {
+    _COVER_WIDTH = 596;
+    this.NUM_COVERS = parseInt(window.innerWidth / (_COVER_WIDTH + 20));
   },
   handleCoverClick: function(key) {
     this.setState({selected: key});
@@ -13,18 +18,24 @@ var Coverflow = React.createClass({
   handleCoverExit: function() {
     this.setState({selected: null});
   },
+  handlePrev: function() {
+
+  },
+  handleNext: function() {
+
+  },
   render: function(){
     if(this.state.selected === null) {
-      var covers = this.props.books.map(function(book, i){
+      var covers = this.props.books.map(function(book.slice(this.state.index, this.NUM_COVERS), i){
         return <Cover book={book} key={i} rKey={i} handleClick={this.handleCoverClick} />
       }, this);
         return (
           <div className="books">
-            <a onClick={this.handlePrev}>&laquo;</a>
             <ul>
-            {covers}
+              <li><a onClick={this.handlePrev}>&laquo;</a></li>
+              {covers}
+              <li><a onClick={this.handleNext}>&raquo;</a></li>
             </ul>
-            <a onClick={this.handleNext}>&raquo;</a>
           </div>
         );
     } else {
