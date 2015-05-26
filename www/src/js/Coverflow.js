1cdc7ddab12205d487d4f427e78f9562aee118d6
Gabe Stein <gabe@ghscommunications.com>
2015-05-26 17:25:46 -0400

    strip out small to create new component

--- www/src/js/Coverflow.js
+++ www/src/js/Coverflow.js
@@ -9,38 +9,24 @@ var Coverflow = React.createClass({
       coverIndex: 0,
       coverLeft: 0,
       translate: 0,
-      smallTranslate: 0,
       transition: {}
     }
   },
   componentWillMount: function() {
-
     this.COVER_WIDTH = 300 + 32;
-    this.SMALL_COVER_WIDTH = 150 + 32;
-
     this.WIDTH = window.innerWidth;
-
     this.NUM_COVERS = Math.round(this.WIDTH / this.COVER_WIDTH) + 1;
-    this.SMALL_NUM_COVERS = Math.round(this.WIDTH / this.SMALL_COVER_WIDTH) + 1;
-
     this.OFFSET = ((this.WIDTH % this.COVER_WIDTH)) / 2;
-    this.SMALL_COVER_WIDTHOFFSET = ((this.WIDTH % this.SMALL_COVER_WIDTH)) / 2;
-
     this.TRANSLATE = Math.round(- (this.COVER_WIDTH * 2) + (this.COVER_WIDTH / this.NUM_COVERS));
-    this.SMALL_TRANSLATE = Math.round(- (this.SMALL_COVER_WIDTH * 2) + (this.SMALL_COVER_WIDTH / this.SMALL_NUM_COVERS));
-
     this.DRAG_EVENT = false;
+    this.SWIPES = 1;
   },
   componentDidMount: function() {
-    this.refs.large.getDOMNode().style.webkitTransform = "translate(" + this.state.translate + "px)";
-    this.refs.small.getDOMNode().style.webkitTransform = "translate(" + this.state.translate + "px)";
+    this.refs.swipeable.getDOMNode().style.webkitTransform = "translate(" + this.state.translate + "px)";
   },
   componentDidUpdate: function() {
-    if(this.refs.large) {
-      this.refs.large.getDOMNode().style.webkitTransform = "translate(" + this.state.translate + "px)";
-    }
-    if(this.refs.small) {
-      this.refs.small.getDOMNode().style.webkitTransform = "translate(" + this.state.translate + "px)";
+    if(this.refs.swipeable) {
+      this.refs.swipeable.getDOMNode().style.webkitTransform = "translate(" + this.state.translate + "px)";
     }
   },
   handleCoverClick: function(key) {
@@ -76,22 +62,18 @@ var Coverflow = React.createClass({
   },
   handleLeft: function(e, x) {
     var translate = this.state.translate - x;
-    this.refs.large.getDOMNode().style.webkitTransform = "translate(" + translate + "px)";
-    this.refs.small.getDOMNode().style.webkitTransform = "translate(" + translate + "px)";
+    this.refs.swipeable.getDOMNode().style.webkitTransform = "translate(" + translate + "px)";
 
   },
   handleRight: function(e, x) {
     var translate = this.state.translate + (x);
-    this.refs.large.getDOMNode().style.webkitTransform = "translate(" + translate + "px)";
-    this.refs.small.getDOMNode().style.webkitTransform = "translate(" + translate + "px)";
+    this.refs.swipeable.getDOMNode().style.webkitTransform = "translate(" + translate + "px)";
 
   },
   snapTo: function(translation) {
 
     var snap = (Math.round(translation / this.COVER_WIDTH) * this.COVER_WIDTH) + this.OFFSET;
-    this.refs.large.getDOMNode().style.webkitTransform = "translate(" + snap + "px)";
-    this.refs.small.getDOMNode().style.webkitTransform = "translate(" + snap + "px)";
-
+    this.refs.swipeable.getDOMNode().style.webkitTransform = "translate(" + snap + "px)";
     this.setState({translate: snap});
   },
   handleSwiped: function(e, x, y, isFlick) {
@@ -113,12 +95,7 @@ var Coverflow = React.createClass({
   },
   render: function(){
     if(this.state.selected === null) {
-      large = this.props.books.slice(0, this.state.coverIndex + this.NUM_COVERS + 2).map(function(book, i){
-        return (
-          <Cover book={book} key={i} rKey={i} handleClick={this.handleCoverClick} />
-          )
-      }, this);
-      small = this.props.books.slice(0, this.state.coverIndex + this.SMALL_NUM_COVERS + 2).map(function(book, i){
+      bigCovers = this.props.books.slice(0, this.state.coverIndex + this.NUM_COVERS + 2).map(function(book, i){
         return (
           <Cover book={book} key={i} rKey={i} handleClick={this.handleCoverClick} />
           )
@@ -126,24 +103,17 @@ var Coverflow = React.createClass({
         return (
           <div className="books">
             <Swipeable
-            className="large"
+            className="swipeable"
             onSwipingLeft={this.handleLeft}
             onSwipingRight={this.handleRight}
             onSwiped={this.handleSwiped}
             flickThreshold={0}
-            ref="large"
+            ref="swipeable"
             style={{WebkitTransition: '-webkit-transform 300ms cubic-bezier(0.175, 0.885, 0.32, 1.275)'}}>
             <ul>
-              {large}
+              {bigCovers}
             </ul>
             </Swipeable>
-            <div ref="small">
-            <ul className="little">
-              {small}
-            </ul>
-            </div>
-            <br /><br />
-            <a href="#" onClick={this.handlePrev}>Prev</a> | <a href="#" onClick={this.handleNext}>Next</a>
           </div>
         );
     } else {
