58c1911b059f4025b51a45670cd5a5e077ad0e05
Gabe Stein <gabe@ghscommunications.com>
2015-05-26 17:12:00 -0400

    second row of books

--- www/src/js/App.js
+++ www/src/js/App.js
@@ -1,5 +1,33 @@
 window.React = require('react');
 var Coverflow = require('./Coverflow');
 var books = require('./books');
+var _ = require('lodash');
+
+var letters = [];
+
+books.sort(compareTitles);
+
+for (i = 0; i < books.length; i++) {
+  new_name = books[i].name.replace('The ', '');
+  letters.push(new_name.slice(0,1));
+}
+
+letters = _.uniq(letters, true);
+
+console.log(letters);
+
+function compareTitles(a, b) {
+  aname = a.name.replace('The ', '');
+  bname = b.name.replace('The ', '');
+  if (aname < bname) {
+    return -1;
+  }
+  if (aname > bname) {
+    return 1;
+  }
+  if (aname == bname) {
+    return 0;
+  }
+}
 
 React.render(<Coverflow books={books} />, document.getElementById('app'));
--- www/src/js/Coverflow.js
+++ www/src/js/Coverflow.js
@@ -9,25 +9,38 @@ var Coverflow = React.createClass({
       coverIndex: 0,
       coverLeft: 0,
       translate: 0,
+      smallTranslate: 0,
       transition: {}
     }
   },
   componentWillMount: function() {
-    this.COVER_WIDTH = 400 + 32;
+
+    this.COVER_WIDTH = 300 + 32;
+    this.SMALL_COVER_WIDTH = 150 + 32;
+
     this.WIDTH = window.innerWidth;
+
     this.NUM_COVERS = Math.round(this.WIDTH / this.COVER_WIDTH) + 1;
+    this.SMALL_NUM_COVERS = Math.round(this.WIDTH / this.SMALL_COVER_WIDTH) + 1;
+
     this.OFFSET = ((this.WIDTH % this.COVER_WIDTH)) / 2;
-    console.log(this.OFFSET);
+    this.SMALL_COVER_WIDTHOFFSET = ((this.WIDTH % this.SMALL_COVER_WIDTH)) / 2;
+
     this.TRANSLATE = Math.round(- (this.COVER_WIDTH * 2) + (this.COVER_WIDTH / this.NUM_COVERS));
+    this.SMALL_TRANSLATE = Math.round(- (this.SMALL_COVER_WIDTH * 2) + (this.SMALL_COVER_WIDTH / this.SMALL_NUM_COVERS));
+
     this.DRAG_EVENT = false;
-    this.SWIPES = 1;
   },
   componentDidMount: function() {
-    this.refs.swipeable.getDOMNode().style.webkitTransform = "translate(" + this.state.translate + "px)";
+    this.refs.large.getDOMNode().style.webkitTransform = "translate(" + this.state.translate + "px)";
+    this.refs.small.getDOMNode().style.webkitTransform = "translate(" + this.state.translate + "px)";
   },
   componentDidUpdate: function() {
-    if(this.refs.swipeable) {
-      this.refs.swipeable.getDOMNode().style.webkitTransform = "translate(" + this.state.translate + "px)";
+    if(this.refs.large) {
+      this.refs.large.getDOMNode().style.webkitTransform = "translate(" + this.state.translate + "px)";
+    }
+    if(this.refs.small) {
+      this.refs.small.getDOMNode().style.webkitTransform = "translate(" + this.state.translate + "px)";
     }
   },
   handleCoverClick: function(key) {
@@ -63,18 +76,22 @@ var Coverflow = React.createClass({
   },
   handleLeft: function(e, x) {
     var translate = this.state.translate - x;
-    this.refs.swipeable.getDOMNode().style.webkitTransform = "translate(" + translate + "px)";
+    this.refs.large.getDOMNode().style.webkitTransform = "translate(" + translate + "px)";
+    this.refs.small.getDOMNode().style.webkitTransform = "translate(" + translate + "px)";
 
   },
   handleRight: function(e, x) {
     var translate = this.state.translate + (x);
-    this.refs.swipeable.getDOMNode().style.webkitTransform = "translate(" + translate + "px)";
+    this.refs.large.getDOMNode().style.webkitTransform = "translate(" + translate + "px)";
+    this.refs.small.getDOMNode().style.webkitTransform = "translate(" + translate + "px)";
 
   },
   snapTo: function(translation) {
 
     var snap = (Math.round(translation / this.COVER_WIDTH) * this.COVER_WIDTH) + this.OFFSET;
-    this.refs.swipeable.getDOMNode().style.webkitTransform = "translate(" + snap + "px)";
+    this.refs.large.getDOMNode().style.webkitTransform = "translate(" + snap + "px)";
+    this.refs.small.getDOMNode().style.webkitTransform = "translate(" + snap + "px)";
+
     this.setState({translate: snap});
   },
   handleSwiped: function(e, x, y, isFlick) {
@@ -96,7 +113,12 @@ var Coverflow = React.createClass({
   },
   render: function(){
     if(this.state.selected === null) {
-      covers = this.props.books.slice(0, this.state.coverIndex + this.NUM_COVERS + 2).map(function(book, i){
+      large = this.props.books.slice(0, this.state.coverIndex + this.NUM_COVERS + 2).map(function(book, i){
+        return (
+          <Cover book={book} key={i} rKey={i} handleClick={this.handleCoverClick} />
+          )
+      }, this);
+      small = this.props.books.slice(0, this.state.coverIndex + this.SMALL_NUM_COVERS + 2).map(function(book, i){
         return (
           <Cover book={book} key={i} rKey={i} handleClick={this.handleCoverClick} />
           )
@@ -104,17 +126,22 @@ var Coverflow = React.createClass({
         return (
           <div className="books">
             <Swipeable
-            className="swipeable"
+            className="large"
             onSwipingLeft={this.handleLeft}
             onSwipingRight={this.handleRight}
             onSwiped={this.handleSwiped}
             flickThreshold={0}
-            ref="swipeable"
+            ref="large"
             style={{WebkitTransition: '-webkit-transform 300ms cubic-bezier(0.175, 0.885, 0.32, 1.275)'}}>
             <ul>
-              {covers}
+              {large}
             </ul>
             </Swipeable>
+            <div ref="small">
+            <ul className="little">
+              {small}
+            </ul>
+            </div>
             <br /><br />
             <a href="#" onClick={this.handlePrev}>Prev</a> | <a href="#" onClick={this.handleNext}>Next</a>
           </div>
