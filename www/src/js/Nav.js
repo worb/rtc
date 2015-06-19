abd8b21574f202a5fb623a674bb59a8df1e712a2
Allan Lasser <lasser.allan@gmail.com>
2015-06-19 01:13:49 -0400

    refactors modal into a subcomponent of nav

--- www/src/js/Nav.js
+++ www/src/js/Nav.js
@@ -1,29 +1,44 @@
+var Modal = require('./Modal');
+
 var Nav = React.createClass({
-  handleAboutClick: function() {
-    this.props.handleModal();
+  getInitialState: function() {
+    return {
+      modalIsVisible: false
+    }
+  },
+  showModal: function(e) {
+    e.preventDefault();
+    this.setState({ modalIsVisible: true });
+  },
+  hideModal: function() {
+    this.setState({ modalIsVisible: false });
   },
   handleExit: function() {
     this.props.handleExit()
   },
   render: function(){
-      return (
-        <nav className="main">
-          <div className="brand" onClick={this.handleExit}>
-            <img src="rtc-logo-stacked.png" />
-          </div>
-          <ul>
-            <li>
-              <a href="#" onClick={this.handleAboutClick}>About</a>
-            </li>
-            <li>
-              <a href="http://shop.thecreativeactionnetwork.com/collections/recovering-the-classics" target="_blank">Shop</a>
-            </li>
-            <li>
-              <a href="http://thecreativeactionnetwork.com/contribute/recovering-the-classics" target="_blank">Submit</a>
-            </li>
-          </ul>
-        </nav>
-      );
+    var modal;
+    if(this.state.modalIsVisible) {
+      modal = <Modal hideModal={this.hideModal} />
+    } else {
+      modal = "";
+    }
+    var shopURL = "http://shop.thecreativeactionnetwork.com/collections/recovering-the-classics";
+    var submitURL = "http://thecreativeactionnetwork.com/contribute/recovering-the-classics";
+    var rtcLogoImageURL = "rtc-logo-stacked.png";
+    return (
+      <nav className="main">
+        {modal}
+        <div className="brand" onClick={this.handleExit}>
+          <img src={rtcLogoImageURL} />
+        </div>
+        <ul>
+          <li><a href="#" onClick={this.showModal}>About</a></li>
+          <li><a href={shopURL} target="_blank">Shop</a></li>
+          <li><a href={submitURL} target="_blank">Submit</a></li>
+        </ul>
+      </nav>
+    );
   }
 });
 
