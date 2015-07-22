9be2e109c332b9f5c574e77b70d636c8f66c2e16
Gabe Stein <gabe@ghscommunications.com>
2015-07-22 16:15:50 -0600

    adds initial lazy loading of images (ie, half of occlusion culling)

--- www/jsx/BookCollectionView.jsx
+++ www/jsx/BookCollectionView.jsx
@@ -16,7 +16,9 @@ var BookCollectionView = React.createClass({
   getInitialState: function() {
     return {
       largeScrollX: 0,
+      smallScrollX: 0,
       largeCoverWidth: 400,
+      smallCoverWidth: 230,
       pageWidth: window.innerWidth,
       selected: null,
       grid: false,
@@ -29,19 +31,37 @@ var BookCollectionView = React.createClass({
   viewCollection: function() {
     this.setState({selected: null});
   },
-  handleLargeScroll: function() {
+  handleScroll: function() {
+     // ComponentDidMount isn't getting proper values, so we do this setup when someone scrolls.
      if(!this.state.scrolled) {
          this.setState({
              scrolled: true
          });
+         // Gets the width of the covers from the dom.
+         console.log(document.querySelector('.small li.cover').offsetWidth)
          this.setState({
-             largeCoverWidth: document.querySelector('.large li.cover').offsetWidth
+             largeCoverWidth: document.querySelector('.large li.cover').offsetWidth,
+             smallCoverWidth: document.querySelector('.small li.cover').offsetWidth
          });
+         // Sets the width of the gallery ul so their containers overflow and show appropriate scrollbars
+         // TODO: figure out why the small UL is ending up too large?
          document.querySelector('.large.covers').style.width = this.state.largeCoverWidth * this.props.books.length + 'px';
+         document.querySelector('.small.covers').style.width = this.state.smallCoverWidth * this.props.books.length + 'px';
      }
-     this.setState({
-         largeScrollX: this.refs.large.getDOMNode().scrollLeft
-     });
+
+     // Detects if the scroll was on the small or the large, and updates state appropriately
+     // TODO: sync scroll
+     if(React.findDOMNode(this.refs.large).scrollLeft !== this.state.largeScrollX) {
+         this.setState({
+             largeScrollX: React.findDOMNode(this.refs.large).scrollLeft
+         });
+     } else if(React.findDOMNode(this.refs.small).scrollLeft !== this.state.smallScrollX) {
+         this.setState({
+             smallScrollX: React.findDOMNode(this.refs.small).scrollLeft
+         });
+     } else {
+
+     }
   },
   render: function(){
     display_mode = this.state.grid ? 'grid' : 'scroll';
@@ -55,16 +75,19 @@ var BookCollectionView = React.createClass({
 
     if(this.state.selected == null) {
 
+      // Sets the slice position for the covers, with offsets to account for starting edge case.
       var largeStart = 0;
       var largeEnd = 1 + Math.round((this.state.pageWidth + this.state.largeScrollX) / this.state.largeCoverWidth);
+      var smallStart = 0;
+      var smallEnd = 4 + Math.round((this.state.pageWidth + this.state.smallScrollX) / this.state.smallCoverWidth);
 
       return (
         <div className="gallery">
-            <div className="large container" ref="large" onScroll={this.handleLargeScroll}>
-                <ul className="large covers">{covers.slice(start, end)}</ul>
+            <div className="large container" ref="large" onScroll={this.handleScroll}>
+                <ul className="large covers">{covers.slice(largeStart, largeEnd)}</ul>
             </div>
-            <div className="small container">
-                <ul className={"small covers " + display_mode}>{covers}</ul>
+            <div className="small container" ref="small" onScroll={this.handleScroll}>
+                <ul className={"small covers " + display_mode}>{covers.slice(smallStart, smallEnd)}</ul>
             </div>
         </div>
       );
