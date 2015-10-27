1943aa48fac851b8329194eac557abe648172025
Gabe Stein <gabe@ghscommunications.com>
2015-10-27 18:48:49 -0400

    adds modal for get books

--- www/jsx/Nav.jsx
+++ www/jsx/Nav.jsx
@@ -2,7 +2,7 @@
 
 var React = require('react/addons');
 
-var Modal = React.createClass({
+var AboutModal = React.createClass({
   // A modal dialog box containing information about the gallery
   // and about the Recovering the Classics project.
   hideModal: function(e) {
@@ -34,27 +34,62 @@ var Modal = React.createClass({
   }
 });
 
+var GetModal = React.createClass({
+  // A modal dialog box containing information about the gallery
+  // and about the Recovering the Classics project.
+  hideModal: function(e) {
+    e.preventDefault();
+    this.props.hideModal();
+  },
+  render: function(){
+    // Visibility is controlled through the application of a 'hidden' class
+    // because visibility is appearance and should be controlled via CSS.
+    // That is to say, the information in in the modal should always be
+    // rendered into the DOM, but it usually should not appear to the user.
+    var modalClass = "modal-container";
+    if (!this.props.modalIsVisible) {
+      modalClass += " hidden";
+    }
+    return (
+      <div className={modalClass}>
+        <div className="modal-bg" onClick={this.hideModal}></div>
+        <div className="modal-inner" id="get">
+          <button onClick={this.hideModal} className="exit">Close</button>
+          <p>Interested in buying Recovering the Classics ebooks for your library? We can make custom ebooks with covers by your local artists.</p>
+          <p>Email us at <a href="mailto:hello@recoveringtheclassics.com">hello@recoveringtheclassics.com</a>.</p>
+        </div>
+      </div>
+    );
+  }
+});
+
 var Nav = React.createClass({
   // Provides information about the project, as well as links to other RTC sites.
   // Uses a modal dialog to present information about the page and project.
   // The navigation maintains the state for the modal dialog.
   getInitialState: function() {
     return {
-      modalIsVisible: false
+      aboutModalIsVisible: false,
+      getModalIsVisible: false
     }
   },
-  showModal: function(e) {
+  showAboutModal: function(e) {
     e.preventDefault();
-    this.setState({ modalIsVisible: true });
+    this.setState({ aboutModalIsVisible: true });
+  },
+  showGetModal: function(e) {
+    e.preventDefault();
+    this.setState({ getModalIsVisible: true });
   },
   hideModal: function() {
-    this.setState({ modalIsVisible: false });
+    this.setState({ getModalIsVisible: false, aboutModalIsVisible: false });
   },
   handleExit: function() {
     this.props.handleExit()
   },
   render: function(){
-    var modal = <Modal modalIsVisible={this.state.modalIsVisible} hideModal={this.hideModal} />;
+    var aboutModal = <AboutModal modalIsVisible={this.state.aboutModalIsVisible} hideModal={this.hideModal} />;
+    var getModal = <GetModal modalIsVisible={this.state.getModalIsVisible} hideModal={this.hideModal} />;
     var shopURL = "http://shop.thecreativeactionnetwork.com/collections/recovering-the-classics";
     var contributeURL = "http://thecreativeactionnetwork.com/contribute/recovering-the-classics";
     var rtcLogoImageURL = "assets/images/rtc-logo-stacked.png";
@@ -66,9 +101,11 @@ var Nav = React.createClass({
         <div className="brand" onClick={this.handleExit}>
           <img src={rtcLogoImageURL} />
         </div>
-        {modal}
+        {aboutModal}
+        {getModal}
         <ul>
-          <li><a href="#about" onClick={this.showModal}>About</a></li>
+          <li><a href="#about" onClick={this.showAboutModal}>About</a></li>
+          <li><a href="#get" onClick={this.showGetModal}>Get Books</a></li>
           <li><a href={shopURL} target="_blank">Shop</a></li>
           <li><a href={contributeURL} target="_blank">Contribute</a></li>
           <li><a href={exhibitURL} target="_blank">Exhibit</a></li>
